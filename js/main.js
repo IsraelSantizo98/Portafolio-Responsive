2741ffa3bbaab0382b435393b2a16644ce4454bf
Israel Santizo <joseisraelss.jiss@gmail.com>
2020-05-30 14:56:24 -0600

    Segundo Commit del responsive Design

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
+const ipad = window.matchMedia("screen and (max-width: 767px)");
 const menu = document.querySelector(".menu");
 console.log(menu);
 const burgerButton = document.querySelector("#burger-menu");
 console.log(burgerButton);
 /*Evento*/
-burgerButton.addEventListener("click", hideShow);
+//activa o desactiva
+ipad.addListener(validation);
+//SIN ESTO NO ABRE EL MENU EN CELULAR
+validation(ipad);
+function validation(event) {
+  if (event.matches) {
+    burgerButton.addEventListener("click", hideShow);
+    // console.log(event);
+  } else {
+    burgerButton.removeEventListener("click", hideShow);
+  }
+}
 function hideShow() {
   if (menu.classList.contains("is_active")) {
     menu.classList.remove("is_active");
